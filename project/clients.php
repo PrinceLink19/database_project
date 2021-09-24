300953ae974b56d3a6893600553adab3c969c663
humi534 <72891850+humi534@users.noreply.github.com>
2021-09-24 09:13:53 +0200

    Add files via upload

---
+++ project/clients.php
@@ -0,0 +1,15 @@
+
+
+<?php
+    include_once("header.php");
+    include("includes/dbconn.inc.php");
+    include("sidebar.php");
+?>
+
+<div class="main-content">
+    <a>This page is all about clients</a>
+</div>
+
+<?php
+    include_once("footer.php");
+?>
